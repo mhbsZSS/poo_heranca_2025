b547a6ac23553bb5739a66b4fb466c9c46a2e17a
Marcio Hernani <marcio.hernani@gmail.com>
2025-09-02 00:35:00 +0000

    Aula 01/09/2025

---
+++ src/main/java/app/Main.java
@@ -0,0 +1,12 @@
+package app;
+
+public class Main {
+    public static void main(String[] args) {
+        Tarefa[] tarefas = new Tarefa[5];
+
+        tarefas[0] = new Tarefa(0, "Teste", false);
+        tarefas[4] = new Tarefa(4, "Novo Teste", false)
+
+        System.out.println(tarefas[0].getDescricao());
+    }
+}
