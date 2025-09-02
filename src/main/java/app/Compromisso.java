fd45995cc0b773e777ed2998c52706a1cef0cfda
Marcio Hernani <marcio.hernani@gmail.com>
2025-09-02 01:26:09 +0000

    Aula 01/09/2025

---
+++ src/main/java/app/Compromisso.java
@@ -0,0 +1,45 @@
+package app;
+
+public class Compromisso extends Atividade {
+    private String dataInicial;
+    private String dataFinal;
+
+    public String getDataInicial(){
+        return dataInicial;
+    }
+
+    public void setDataInicial(String dataInicial) {
+        this.dataInicial = dataInicial;
+    }
+
+    public String getDataFinal(){
+        return dataFinal;
+    }
+
+    public void setDataFinal(String dataFinal) {
+        this.dataFinal = dataFinal;
+    }
+
+    public Compromisso(
+        long id, 
+        String descricao, 
+        String dataInicial, 
+        String dataFinal) {
+
+            this.setId(id);
+            this.setDescricao(descricao);
+            this.setDataInicial(dataInicial);
+            this.setDataFinal(dataFinal);
+        }
+    public Compromisso(
+        long id, 
+        String descricao, 
+        String dataInicial) {
+
+            this.setId(id);
+            this.setDescricao(descricao);
+            this.setDataInicial(dataInicial);
+        }
+    
+    public Compromisso() {}
+}
--- src/main/java/app/Tarefa.java
+++ src/main/java/app/Tarefa.java
@@ -1,27 +1,9 @@
 package app;
 
-public class Tarefa {
-    private long id;
-    private String descricao;
+public class Tarefa extends Atividade {
     private boolean isConcluido;
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-     public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-     public boolean isConcluido() {
+    public boolean isConcluido() {
         return isConcluido;
     }
 
